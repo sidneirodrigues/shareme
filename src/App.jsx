e1fbdd50748a20b1abe8e10d2dc28a73a23bf2b9
Sidnei Rodrigues <sidnei.c.rodrigues@hotmail.com>
2022-11-15 07:32:16 -0300

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {  BrowserRouter as Router, Route, UseNavigate } from 'react-router-dom'
+
+import Home from './container/Home';
+import Login from './components/Login';
+import './App.css';
 
 function App() {
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-  </div>
+    <Router>
+      <UseNavigate>
+        <Route path='/*' component={Home} exact />
+        <Route path='/login' component={Login} />
+      </UseNavigate>
+    </Router>
   )
 }
 
